e8ea08945226ac335d6a2ca8ccab96095fa39e89
Andres Jaramillo <a.jaramillo24@gmail.com>
2021-09-28 00:10:54 +0000

    monkey range

--- src/app.js
+++ src/app.js
@@ -9,3 +9,31 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+function createPronounAndAdj(finalWebsite) {
+  const pronounAndAdj = [];
+  let newWord = "";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        newWord = pronoun[i] + adj[j] + noun[k] + finalWebsite;
+        pronounAndAdj.push(newWord);
+      }
+    }
+  }
+
+  return pronounAndAdj;
+}
+
+let comSites = createPronounAndAdj(".com");
+console.log(comSites);
+let netSites = createPronounAndAdj(".net");
+let govSites = createPronounAndAdj(".gov");
+
+let allSites = comSites.concat(netSites, govSites);
+
+console.log(allSites);
