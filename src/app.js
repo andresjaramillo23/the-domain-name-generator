c87d0dadaa22313623c89b119c274e0ea767741b
Andres Jaramillo <a.jaramillo24@gmail.com>
2021-09-29 04:53:33 +0000

    Added html style for displaying websites

--- src/app.js
+++ src/app.js
@@ -13,27 +13,92 @@ window.onload = function() {
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
+let websiteTypes = ["com", "gov", "net", "col", "ru"];
+const enteredWords = new Array();
 
-function createPronounAndAdj(finalWebsite) {
-  const pronounAndAdj = [];
-  let newWord = "";
+function websiteGenerator() {
+  const browserNames = [];
+  let browserName = "";
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        newWord = pronoun[i] + adj[j] + noun[k] + finalWebsite;
-        pronounAndAdj.push(newWord);
+        for (let l = 0; l < websiteTypes.length; l++) {
+          browserName = pronoun[i] + adj[j] + noun[k] + "." + websiteTypes[l];
+          browserNames.push(browserName);
+        }
       }
     }
   }
 
-  return pronounAndAdj;
+  return browserNames;
 }
 
-let comSites = createPronounAndAdj(".com");
-console.log(comSites);
-let netSites = createPronounAndAdj(".net");
-let govSites = createPronounAndAdj(".gov");
+function isAlreadyThere(array, word) {
+  let isAlready = false;
+  for (let j = 0; j < array.length; j++) {
+    if (word.toUpperCase() == array[j].toUpperCase()) isAlready = true;
+  }
 
-let allSites = comSites.concat(netSites, govSites);
+  return isAlready;
+}
 
-console.log(allSites);
+let paragraphStyle = "text-center text-uppercase fs-6 fw-bold font-monospace";
+
+function setSentencesInHtml() {
+  let allSites = websiteGenerator();
+  let counter = 0;
+  for (let i = 0; i < allSites.length; i++) {
+    counter++;
+    document.getElementById("firstRow").innerHTML +=
+      "<div class=" +
+      "col-2><p class=" +
+      '"' +
+      paragraphStyle +
+      '"' +
+      ">" +
+      allSites[i] +
+      "</p></div>";
+  }
+}
+
+function resetInputValues() {
+  document.getElementById("inputWord").value = "";
+  document.getElementsByName("myWordsList")[0].value = "";
+}
+
+function populateArrayOfNewWordsEntered(valueInput) {
+  if (valueInput.length > 0) enteredWords.push(valueInput);
+}
+
+function populateSentencesWithNewInput(valueInput, keyInput) {
+  if (valueInput.length > 0) {
+    switch (keyInput) {
+      case "Prounoun":
+        if (!isAlreadyThere(pronoun, valueInput)) pronoun.push(valueInput);
+        break;
+      case "Adjective":
+        if (!isAlreadyThere(adj, valueInput)) adj.push(valueInput);
+        break;
+      case "Noun":
+        if (!isAlreadyThere(noun, valueInput)) noun.push(valueInput);
+        break;
+      case "WebsiteType":
+        if (!isAlreadyThere(websiteTypes, valueInput))
+          websiteTypes.push(valueInput);
+        break;
+    }
+  }
+}
+
+const btn = document.getElementById("b1");
+
+btn.onclick = function() {
+  let valueInput = document.getElementById("inputWord").value;
+  let keyInput = document.getElementsByName("myWordsList")[0].value;
+
+  populateArrayOfNewWordsEntered(valueInput);
+  populateSentencesWithNewInput(valueInput, keyInput);
+
+  resetInputValues();
+  setSentencesInHtml();
+};
